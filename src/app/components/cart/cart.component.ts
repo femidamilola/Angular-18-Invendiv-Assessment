d61b6c41d1cff8a629609ad6eb47704ca25f66bf
Adebayo Oluwafemi Damilola <45465232+femidamilola@users.noreply.github.com>
2025-03-13 16:07:30 +0200

    Updated the responsiveness of the cart component

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, take } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { map } from 'rxjs/operators';
 import { Cart } from '@Models/cart.model';
@@ -8,48 +8,66 @@ import { CartState } from '@Ngrx/reducers/cart.reducer';
 import { removeFromCart } from '@Ngrx/actions/cart.action';
 import { CommonModule } from '@angular/common';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faRemove, faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss'],
-  providers: [NgbActiveModal]
+  providers: [NgbActiveModal],
 })
 export class CartComponent {
+  faCancel = faRemove;
+  faPlus = faPlus;
+  faMinus = faMinus;
   cart$: Observable<Cart[]>;
   subtotal$: Observable<number>;
   discount$ = new BehaviorSubject<number>(0);
   grandTotal$: Observable<number>;
   discountCode = '';
+  invalidDiscount = false;
 
-  constructor(private store: Store<{ cart: CartState }>, private cartService: CartService, public activeModal: NgbActiveModal) {
-    this.cart$ = this.store.select(state => state.cart.items);
+  constructor(
+    private store: Store<{ cart: CartState }>,
+    private cartService: CartService,
+    public activeModal: NgbActiveModal
+  ) {
+    this.cart$ = this.store.select((state) => state.cart.items);
 
     this.subtotal$ = this.cart$.pipe(
-      map(items => items.reduce((acc, item) => acc + item.quantity * item.product.price, 0))
+      map((items) => items.reduce((acc, item) => acc + item.quantity * item.product.price, 0))
     );
 
-    this.cartService.getDiscount().subscribe(discount => {
+    this.cartService.getDiscount().subscribe((discount) => {
       this.discount$.next(discount);
       this.updateGrandTotal();
     });
 
-    this.grandTotal$ = this.subtotal$.pipe(
-      map(subtotal => subtotal - this.discount$.getValue())
-    );
+    this.grandTotal$ = this.subtotal$.pipe(map((subtotal) => subtotal - this.discount$.getValue()));
   }
 
   updateQuantity(productId: number, change: number) {
-    this.store.select(state => state.cart.items).subscribe(cartItems => {
-      const updatedCart = cartItems.map(item =>
-        item.product.id === productId
-          ? { ...item, quantity: Math.max(1, item.quantity + change) } // Prevents negative quantity
-          : item
-      );
-      updatedCart.forEach(item => this.cartService.addProductToCart(item.product)); // Update via store
+    this.store.select(state => state.cart.items).pipe(
+      map(cartItems => {
+        return cartItems.map(item =>
+          item.product.id === productId
+            ? { ...item, quantity: Math.max(1, item.quantity + change) } // Ensure min quantity is 1
+            : item
+        );
+      }),
+      take(1) // Prevent multiple subscriptions
+    ).subscribe(updatedCart => {
+      // Find the updated item
+      const updatedItem = updatedCart.find(item => item.product.id === productId);
+      if (updatedItem) {
+        this.cartService.addProductToCart(updatedItem.product); // Dispatch update for only the affected item
+      }
     });
   }
+
 
   removeItem(productId: number) {
     this.store.dispatch(removeFromCart({ productId }));
@@ -59,11 +77,14 @@ export class CartComponent {
     const input = event.target as HTMLInputElement;
     this.discountCode = input.value.trim();
     this.cartService.applyDiscount(this.discountCode);
+
+    // Check if discount is valid
+    this.cartService.getDiscount().subscribe((discount) => {
+      this.invalidDiscount = discount === 0 && this.discountCode !== '';
+    });
   }
 
   updateGrandTotal() {
-    this.grandTotal$ = this.subtotal$.pipe(
-      map(subtotal => subtotal - this.discount$.getValue())
-    );
+    this.grandTotal$ = this.subtotal$.pipe(map((subtotal) => subtotal - this.discount$.getValue()));
   }
 }
