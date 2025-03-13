d305c7ea44099b11987f1906468628cb97da41a5
Adebayo Oluwafemi Damilola <45465232+femidamilola@users.noreply.github.com>
2025-03-13 06:18:10 +0200

    Updated the application configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CartComponent } from "@Components/cart/cart.component";
+import { ProductListComponent } from "@Components/product-list/product-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CartComponent, ProductListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
