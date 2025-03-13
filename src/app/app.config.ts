000b83d45b140852972fda6df04baf6a1045b7cf
Adebayo Oluwafemi Damilola <45465232+femidamilola@users.noreply.github.com>
2025-03-13 06:16:10 +0200

    Application Setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideStore, provideState } from '@ngrx/store';
+import { cartReducer } from '@Ngrx/reducers/cart.reducer';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideStore(), provideState({ name: 'cart', reducer: cartReducer })]
 };
