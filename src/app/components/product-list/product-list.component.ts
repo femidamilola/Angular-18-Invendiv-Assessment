ed07370e00498528cb6daca5e8d180c66932b130
Adebayo Oluwafemi Damilola <45465232+femidamilola@users.noreply.github.com>
2025-03-13 06:17:31 +0200

    Created the product list component

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BehaviorSubject } from 'rxjs';
+import { Product } from '@Models/product.model';
+import { CartService } from '@Services/cart.service';
+import { Store } from '@ngrx/store';
+import { Observable, map } from 'rxjs';
+import { CartState } from '@Ngrx/reducers/cart.reducer';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.scss'
+  styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent {
+  cartCount$: Observable<number>;
+  faCart = faCartShopping;
+  @Output() cartToggle = new EventEmitter<boolean>();
 
+  private allProducts: Product[] = [
+    { id: 1, name: 'Nike Air Force', price: 760, image: 'nike_air_force.jpg' },
+    { id: 2, name: 'Nike Air Jordan', price: 220, image: 'nike_air_jordan.jpg' },
+    { id: 3, name: 'Adidas Yeezy', price: 140, image: 'adidas_yeezy.jpg' },
+    { id: 4, name: 'Naked Wolf', price: 320.50, image: 'naked_wolf.jpg' },
+    { id: 4, name: 'Reebok', price: 320.50, image: 'reebok.jpg' }
+  ];
+
+  private filteredProducts$ = new BehaviorSubject<Product[]>(this.allProducts);
+  products$ = this.filteredProducts$.asObservable();
+
+  constructor(private cartService: CartService, private store: Store<{ cart: CartState }>) {
+    this.cartCount$ = this.store.select(state => state.cart.items).pipe(
+      map(items => items.length)
+    );
+  }
+
+  filterProducts(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input) {
+      const searchLower = input.value.toLowerCase();
+      const filtered = this.allProducts.filter(product =>
+        product.name.toLowerCase().includes(searchLower)
+      );
+      this.filteredProducts$.next(filtered);
+    }
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addProductToCart(product);
+  }
+  openCartModal() {
+    this.cartToggle.emit(true);
+  }
 }
